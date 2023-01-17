04e50496b56c3818b828ec87404278b2c5eaf2d5
7OMtom <ducatom@gmx.at>
2023-01-17 21:55:58 +0100

    Just added a Comment on Playing Class.

--- src/States/Playing.java
+++ src/States/Playing.java
@@ -33,6 +33,7 @@ public class Playing implements State {
         System.out.println("States.Playing!");
 
         //hier vielleicht eine Base.Game Class die Punkte speichert (und mit Spiel verbunden ist)
+        //Das Spiel gehört ja immer zu einem Spieler und der Spieler ist im Flipper (context) geführt. Man kann also über den Spieler zum Game Punkte speichern. Aber schauen wir mal, ob das nicht der Visitor erledigt :-)
 
         shootBall();
 
